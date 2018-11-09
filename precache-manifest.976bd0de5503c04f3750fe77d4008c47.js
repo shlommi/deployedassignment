5b6e2035d69f321d64b811c458fcfefc35eccc1a
shlommi <shlommi@gmail.com>
2018-11-09 23:38:38 +0200

    Updates

---
+++ precache-manifest.976bd0de5503c04f3750fe77d4008c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a61a6d387e8b2a2f186",
+    "url": "/deployedassignment/static/js/runtime~main.1a61a6d3.js"
+  },
+  {
+    "revision": "209b83ab248c58c0f731",
+    "url": "/deployedassignment/static/js/main.209b83ab.chunk.js"
+  },
+  {
+    "revision": "2b54d213f7b5e75d0a21",
+    "url": "/deployedassignment/static/js/1.2b54d213.chunk.js"
+  },
+  {
+    "revision": "209b83ab248c58c0f731",
+    "url": "/deployedassignment/static/css/main.77e92329.chunk.css"
+  },
+  {
+    "revision": "2b54d213f7b5e75d0a21",
+    "url": "/deployedassignment/static/css/1.5fe88d81.chunk.css"
+  },
+  {
+    "revision": "f0c10b162b3292e92483b36990f8d97b",
+    "url": "/deployedassignment/index.html"
+  }
+];
