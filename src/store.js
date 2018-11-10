915fcfca25066ec8b249c90c8dcf593c6b4265e2
shlommi <shlommi@gmail.com>
2018-11-10 20:33:59 +0200

    fix deploy

--- src/store.js
+++ src/store.js
@@ -2,17 +2,17 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/";
 
-const initialState = {};
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
-const middleware = [thunk];
+const initialState = {};
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
